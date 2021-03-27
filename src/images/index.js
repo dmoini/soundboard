0282aa87e66cf415f35948529a42ffe9fe7e3190
Donovan Moini <donovanmoini@gmail.com>
2021-03-27 16:33:32 -0700

    Added first images

---
+++ src/images/index.js
@@ -0,0 +1,6 @@
+export { default as AnimeWowImage } from "./anime-wow.png";
+export { default as BruhImage } from "./bruh.jpeg";
+export { default as BuzzerImage } from "./buzzer.jpg";
+export { default as NutImage } from "./nut.png";
+export { default as RobloxOofImage } from "./roblox-oof.jpg";
+export { default as YeeImage } from "./yee.jpeg";
