ddc6f2562febabd49fea2b690b89fdfd16926bdc
Donovan Moini <donovanmoini@gmail.com>
2021-03-27 16:33:39 -0700

    Added first sounds

---
+++ src/sounds/index.js
@@ -0,0 +1,6 @@
+export { default as AnimeWowSound } from "./anime-wow.mp3";
+export { default as BruhSound } from "./bruh.mp3";
+export { default as BuzzerSound } from "./buzzer.mp3";
+export { default as NutSound } from "./nut.mp3";
+export { default as RobloxOofSound } from "./roblox-oof.mp3";
+export { default as YeeSound } from "./yee.mp3";
