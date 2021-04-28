df5b28e6de88e0a0299910c2972b1a08c416b363
Donovan Moini <donovanmoini@gmail.com>
2021-04-27 22:52:19 -0700

    Removed "Image" and "Sound" from images and sounds and minor cleanup

--- TODO.md
+++ TODO.md
@@ -2,3 +2,7 @@ SoundButton:
 
 - Make rounded color the average color of SoundButton's image
 - Change text to green while sound is playing and back to white when sound ends
+
+Other:
+
+- Remove scroll bars
--- src/common/buttons.js
+++ src/common/buttons.js
@@ -3,27 +3,28 @@ import * as soundImport from "../sounds";
 
 import categories from "../common/constants";
 
-const CATEGORY_INDEX_CHARACTER = "_";
-const IMAGE_STRING_LENGTH = 5;
+const CATEGORY_INDEX_SEPARATOR = "_";
 
 const zip = (a, b) => a.map((v, i) => [v, b[i]]);
 
 const getName = (s) => {
-  switch (s) {
-    case "EnemyAC130AboveImage":
+  const categoryIndex = s.indexOf(CATEGORY_INDEX_SEPARATOR);
+  const name = categoryIndex !== -1 ? s.substring(0, categoryIndex) : s;
+  switch (name) {
+    case "EnemyAC130Above":
       return "Enemy AC-130 Above";
     default:
-      const categoryIndex = s.indexOf(CATEGORY_INDEX_CHARACTER);
-      const baseImageString =
-        categoryIndex !== -1 ? s.substring(0, categoryIndex - IMAGE_STRING_LENGTH) : s.substring(0, s.length - IMAGE_STRING_LENGTH);
-      return baseImageString.replace(/([A-Z])/g, " $1").trim();
+      return name.replace(/([A-Z])/g, " $1").trim();
   }
 };
 
 const getCategory = (s) => {
-  const categoryIndex = s.indexOf(CATEGORY_INDEX_CHARACTER);
-  const category = categoryIndex !== -1 ? s.substring(categoryIndex + 1) : categories.OTHER;
-  return categories.hasOwnProperty(category) ? categories[category] : categories.OTHER;
+  const categoryIndex = s.indexOf(CATEGORY_INDEX_SEPARATOR);
+  if (categoryIndex !== -1) {
+    const category = s.substring(categoryIndex + 1);
+    return categories.hasOwnProperty(category) ? categories[category] : categories.OTHER;
+  }
+  return categories.OTHER;
 };
 
 const colors = ["#FFFF00", "#FF0000", "#00FF00", "#00FFFF", "#FF00FF", "#9D00FF"];
