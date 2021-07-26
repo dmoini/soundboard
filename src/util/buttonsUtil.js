56713e7f198199392320cefcfe12346e1eeb65d8
Donovan Moini <donovanmoini@gmail.com>
2021-07-26 14:37:54 -0700

    Updated variable assignment to ternary statement

--- src/util/buttonsUtil.js
+++ src/util/buttonsUtil.js
@@ -3,12 +3,9 @@ import { categories, soundNameToCategoryInfoMap, soundNameToCustomNameMap } from
 export const zip = (a, b) => a.map((v, i) => [v, b[i]]);
 
 export const getName = (soundName) => {
-  let returnName = soundName;
-  if (soundNameToCustomNameMap.hasOwnProperty(soundName)) {
-    returnName = soundNameToCustomNameMap[soundName];
-  } else {
-    returnName = soundName.replace(/([A-Z])/g, " $1").trim();
-  }
+  let returnName = soundNameToCustomNameMap.hasOwnProperty(soundName)
+    ? soundNameToCustomNameMap[soundName]
+    : soundName.replace(/([A-Z])/g, " $1").trim();
   if (soundNameToCategoryInfoMap[soundName]?.category === categories.FRIENDS) {
     returnName = `${returnName} (${soundNameToCategoryInfoMap[soundName].friend})`;
   }
